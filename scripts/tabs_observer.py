976b345194e1897673e17df15b0876f0acb8a115
Haoming <hmstudy02@gmail.com>
2024-11-16 15:30:20 +0800

    pain

--- scripts/tabs_observer.py
+++ scripts/tabs_observer.py
@@ -1,9 +1,17 @@
 from modules.script_callbacks import on_after_component
+from gradio import __version__ as gr_version
 from gradio.components import Component
 from gradio import Checkbox
 
 
-JS = "() => { TabsExtension.refreshEnableCheckbox(); }"
+is_gradio_4: bool = int(str(gr_version).split(".", 1)[0]) == 4
+
+
+def js() -> dict:
+    if is_gradio_4:
+        return {"js": "() => { TabsExtension.refreshEnableCheckbox(); }"}
+    else:
+        return {"_js": "() => { TabsExtension.refreshEnableCheckbox(); }"}
 
 
 def observe(component: Component, **kwargs: dict):
@@ -13,12 +21,12 @@ def observe(component: Component, **kwargs: dict):
     elem_id: str = kwargs.get("elem_id", None) or ""
     # InputAccordion
     if "-checkbox" in elem_id:
-        component.change(fn=None, _js=JS)
+        component.change(fn=None, **js())
         return
 
     label: str = (kwargs.get("label", None) or "").lower()
     if "enable" in label or "active" in label:
-        component.change(fn=None, _js=JS)
+        component.change(fn=None, **js())
 
 
 on_after_component(observe)
